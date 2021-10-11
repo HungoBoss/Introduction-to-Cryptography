67ede069c46222d9ec5b9c6fd8910f1c430a57b1
nqhun <ngoqhu@gmail.com>
2021-10-11 21:55:14 +0200

    Finished implementation of the third task

--- 02-Homework.py
+++ 02-Homework.py
@@ -27,6 +27,30 @@ def is_prime(n):
         print(f"{n} is NOT a prime number!")
 
 
+def calculateGCD(x, y):
+    """
+    Calculates and returns the GCD value of X and Y.
+    """
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    
+    for i in range(1, smaller + 1):
+        if ((x % i == 0) and (y % i == 0)):
+            gcd = i
+    
+    return gcd
+
+
+def calculateLCM(x, y):
+    """
+    Calculates and returns the LCM value of X and Y.
+    LCM can be calculated by the multiplication of X and Y devided by the GCD of those two numbers.
+    """
+    return (x * y / calculateGCD(x, y))
+
+
 def main():
     """
     Main function containing all finished tasks.
@@ -34,6 +58,10 @@ def main():
 
     # First task
     is_prime(73241)     # shouldn't be a prime number
+
+    # Second task
+    print(f"GCD: {calculateGCD(6083, 68651)}")
+    print(f"LCM: {calculateLCM(6083, 68651)}")
 
 
 if __name__ == "__main__":
