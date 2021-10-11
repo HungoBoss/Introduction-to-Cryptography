e25e1e5001b6209adb7a7fae7d040294d5ca3f3f
nqhun <ngoqhu@gmail.com>
2021-10-11 21:41:55 +0200

    Finished implementation of the first task

--- 02-Homework.py
+++ 02-Homework.py
@@ -5,11 +5,36 @@
 #                           #
 # # # # # # # # # # # # # # #
 
+from math import sqrt
+
+def is_prime(n):
+    """
+    Determines whether a given number is a prime or not. Prints the result into the console.
+    """
+    prime = False
+
+    if n > 1:
+        for i in range(2, int(sqrt(n)) + 1):
+            if (n % i == 0):
+                prime = True
+                break
+        
+        if prime:
+            print(f"{n} is NOT a prime number!")
+        else:
+            print(f"{n} is prime number!")
+    else:
+        print(f"{n} is NOT a prime number!")
+
+
 def main():
     """
     Main function containing all finished tasks.
     """
-    pass
+
+    # First task
+    is_prime(73241)     # shouldn't be a prime number
+
 
 if __name__ == "__main__":
     main()
