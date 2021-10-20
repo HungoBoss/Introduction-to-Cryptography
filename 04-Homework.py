7168f2dc26ab728a5f319ed22602a767d31ecd7e
nqhun <ngoqhu@gmail.com>
2021-10-20 20:30:00 +0200

    Created a file for fourth assignment

---
+++ 04-Homework.py
@@ -0,0 +1,15 @@
+# # # # # # # # # # # # # # #
+#                           #
+#   BPC-ZKR Assignment 04   #
+#   Author: Hung Ngo Quang  #
+#                           #
+# # # # # # # # # # # # # # #
+
+def main():
+    """
+    Main function containing all finished tasks.
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
