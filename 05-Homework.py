c33bcde1829aea15d6d7416512d1d8409c78a4a0
nqhun <ngoqhu@gmail.com>
2021-10-27 22:06:00 +0200

    Finished implementation of the first task

--- 05-Homework.py
+++ 05-Homework.py
@@ -6,7 +6,11 @@
 # # # # # # # # # # # # # # #
 
 def main():
-    pass
+    # First task
+    print("1) ")
+    print("Budeme vycházet z Malé Fermatovy věty:\n\tx^(p-1) ≡ 1 (mod p)")
+    print("Obě dvě strany můžeme vynásobit x^(-1), čímž dostaneme:\n\tx^(p-2) ≡ x^(-1) mod p")
+    print("Převedením (mod p) na levou stranu (komutativní operace), pak získáme vztah:\n\tx^(p-2) mod p ≡ x^(-1)")
 
 if __name__ == "__main__":
     main()
