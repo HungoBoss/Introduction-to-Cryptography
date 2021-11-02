e968e678800599122675780096683ab1140694c3
nqhun <ngoqhu@gmail.com>
2021-11-02 15:44:08 +0100

    Created a file for sixth assignment

---
+++ 06-Homework.py
@@ -0,0 +1,12 @@
+# # # # # # # # # # # # # # #
+#                           #
+#   BPC-ZKR  Assignment 05  #
+#   Author: Hung Ngo Quang  #
+#                           #
+# # # # # # # # # # # # # # #
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
