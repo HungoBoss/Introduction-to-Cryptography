c68050b462b0bd724f86a0b38afc859741d34196
nqhun <ngoqhu@gmail.com>
2021-11-02 16:07:00 +0100

    Finished implementation of the first task

--- 06-Homework.py
+++ 06-Homework.py
@@ -5,8 +5,21 @@
 #                           #
 # # # # # # # # # # # # # # #
 
+def basic_euclidean_algorithm(a, b):
+    """
+    Recursive function that calculates the GCD of given numbers a and b using a basic version of Euclidean algorithm. Returns the value of GCD.
+    """
+    if a == 0:
+        return b
+    
+    return basic_euclidean_algorithm(b % a, a)
+
+
 def main():
-    pass
+    # First task
+    print("1) ")
+    print(f"GCD(10547, 1651) = {basic_euclidean_algorithm(10547, 1651)}")
+
 
 if __name__ == "__main__":
     main()
