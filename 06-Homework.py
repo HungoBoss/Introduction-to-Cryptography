d0a631c5f6487fb81581c278ed782a2a19f30b00
nqhun <ngoqhu@gmail.com>
2021-11-02 16:23:43 +0100

    Finished implementation of the second task

--- 06-Homework.py
+++ 06-Homework.py
@@ -1,6 +1,6 @@
 # # # # # # # # # # # # # # #
 #                           #
-#   BPC-ZKR  Assignment 05  #
+#   BPC-ZKR  Assignment 06  #
 #   Author: Hung Ngo Quang  #
 #                           #
 # # # # # # # # # # # # # # #
@@ -15,10 +15,26 @@ def basic_euclidean_algorithm(a, b):
     return basic_euclidean_algorithm(b % a, a)
 
 
+def extended_euclidean_algorithm(a, b):
+    """
+    Calculate the inverse element using an Extended Euclidean algorithm.
+    """
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = extended_euclidean_algorithm(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+
 def main():
     # First task
-    print("1) ")
+    print("1)")
     print(f"GCD(10547, 1651) = {basic_euclidean_algorithm(10547, 1651)}")
+    
+    # Second task
+    print("2)")
+    _, x, _ = extended_euclidean_algorithm(345, 577)
+    print(f"345^(-1) mod 577 = {x % 577}")
 
 
 if __name__ == "__main__":
