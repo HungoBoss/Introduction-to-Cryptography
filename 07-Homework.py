93a54711dd906c9d9fb43ba51097006a24803cb8
nqhun <ngoqhu@gmail.com>
2021-11-12 17:54:42 +0100

    Created and initialized file for the seventh assignment

---
+++ 07-Homework.py
@@ -0,0 +1,13 @@
+# # # # # # # # # # # # # # #
+#                           #
+#   BPC-ZKR  Assignment 07  #
+#   Author: Hung Ngo Quang  #
+#                           #
+# # # # # # # # # # # # # # #
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
