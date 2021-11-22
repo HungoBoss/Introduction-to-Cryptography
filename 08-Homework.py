d2e632b85a46045c36b1dbb7df00b5e1b2fc5591
nqhun <ngoqhu@gmail.com>
2021-11-22 23:43:14 +0100

    Created a file for the eight assignment

---
+++ 08-Homework.py
@@ -0,0 +1,12 @@
+# # # # # # # # # # # # # # #
+#                           #
+#   BPC-ZKR  Assignment 08  #
+#   Author: Hung Ngo Quang  #
+#                           #
+# # # # # # # # # # # # # # #
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
