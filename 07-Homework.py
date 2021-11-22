1fb27b50484bd89d6123a725981066798b295298
nqhun <ngoqhu@gmail.com>
2021-11-22 22:25:50 +0100

    Finished implementation of the third task

--- 07-Homework.py
+++ 07-Homework.py
@@ -7,6 +7,14 @@
 
 from Crypto.PublicKey import DSA
 
+
+def find_q(p, g):
+    for i in range(1, p):
+        if g ** i % p == 1:
+            return i
+    return -1
+
+
 def DSA_parameters():
     key = DSA.generate(1024)
     p, q, g = key.domain()
@@ -24,10 +32,17 @@ def main():
     # First task
     print("1)")
     print_recommendation_for_DSA()
+    print()
 
     # Second task
     print("2)")
     DSA_parameters()
+    print()
+
+    # Third task
+    print("3)")
+    print(f"q = {find_q(29, 13)}")
+    print()
 
 
 if __name__ == "__main__":
